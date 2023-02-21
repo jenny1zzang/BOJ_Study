c948ade254fbc771b3c64d267dbcf264bc337b7e
jenny1zzang <cyberkhl@naver.com>
2023-02-21 14:19:58 +0900

    20230220

---
+++ 20230220_20230226/10818.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N, n, i;
+    int max, min;
+    scanf("%d",&N);
+
+    for (i=0;i<N;i++){
+        scanf("%d",&n);
+        if (i==0) {
+            max = n;
+            min = n;
+        }
+
+        else {
+            if (max<n) max = n;
+            else if (min > n) min = n;
+        }
+    }
+    printf("%d %d",min, max);
+}
---
+++ 20230220_20230226/11720.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    char number;
+    int total=0;
+    scanf("%d",&N);
+    getchar();
+    for (i=0;i<N;i++){
+        scanf("%c",&number);
+
+        total += number - '0';
+    }
+    
+    printf("%d",total);
+
+    return 0;
+}
